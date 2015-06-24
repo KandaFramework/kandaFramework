b9658d4890ec0e3cf7b971ea27726add91693ae8
Marcu5s <marcusjavascript@gmail.com>
2015-06-24 14:47:23 -0300

    Correção Autoload

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -6,7 +6,7 @@ function Autoload($class) {
     $class = WWW_ROOT.DS. str_replace("/", DS, $class) . '.php';
   
 
-    echo $class.'<br/>';
+    echo $class."<br/>";
 
     $array =  explode('/',$class);
     $array = array_filter($array);
