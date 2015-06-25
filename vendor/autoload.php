de772e545af39be5efbeb385f8a8fa7e04f9ad5b
Marcu5s <marcusjavascript@gmail.com>
2015-06-25 17:39:35 -0300

    Correcao

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -39,6 +39,9 @@ function Autoload($class) {
     }
     $filename = $dilimitador.implode(DS,$array);
      
+
+    echo $filename;
+
     if(!file_exists($filename))
         throw new Exception("File path $class not found.");
     
